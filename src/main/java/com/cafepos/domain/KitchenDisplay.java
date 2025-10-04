6a80561b335bb91e7717c3f1cf281fc86eaf4f1f
Sharon Akachi-Kanu <sakachikanu@gmail.com>
2025-10-04 17:58:24 +0100

    showing orderItem

--- src/main/java/com/cafepos/domain/KitchenDisplay.java
+++ src/main/java/com/cafepos/domain/KitchenDisplay.java
@@ -3,14 +3,14 @@ package com.cafepos.domain;
 public final class KitchenDisplay implements OrderObserver {
     @Override
     public void updated(Order order, String eventType) {
-        // TODO: on "itemAdded" -> print "[Kitchen] Order
-        /*
-         * #<id>: item added"
-         * // on "paid" -> print "[Kitchen] Order
-         * #<id>: payment received"
-         */
         if (eventType.equals("itemAdded")) {
-            System.out.println("[Kitchen] Order #" + order.id() + ": item added");
+            var items = order.items();
+            if (!items.isEmpty()) {
+                LineItem lastItem = items.get(items.size() - 1);
+                System.out.println("[Kitchen] Order #" + order.id() + ": " + 
+                                   lastItem.quantity() + "x " + 
+                                   lastItem.product().name() + " added");
+            }
         } else if (eventType.equals("paid")) {
             System.out.println("[Kitchen] Order #" + order.id() + ": Payment received");   
         }
